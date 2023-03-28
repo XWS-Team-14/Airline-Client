ea4480ab5a26e905451dae0d697b60240ebe629d
BogdanDavinic <davinic.ra153.2019@uns.ac.rs>
2023-03-28 13:29:27 +0200

    fix: delete feature, camel case, typos
    Delete feature now works properly,
    camel case has been fixed in files,
    fixed typos, and deleted unnecessary comments.

--- src/features/flights/components/AddFlight.tsx
+++ src/features/flights/components/AddFlight.tsx
@@ -12,61 +12,63 @@ import 'react-toastify/dist/ReactToastify.css';
 import { addFlight } from '../services/flight.service';
 import styles from '../styles/AllFlights.module.scss';
 import FlightDto from '../types/FlightDto';
-import {ArrowRightOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined } from '@ant-design/icons';
 const AddFlight = () => {
   const [form] = Form.useForm();
   const router = useRouter();
-  const [Routes, setRoutes] = useState<any[]>([]);
+  const [routes, setRoutes] = useState<any[]>([]);
   const [selectedRoute, setSelectedRoute] = useState<any>(null);
-    useEffect (() =>  {
-    api
-    .get('api/route/all/places/')
-    .then((res) =>{
-        setRoutes(Array.from(res.data.results));
-    })}, [])
-  const onFinish = (values: FlightDto) => {
+  useEffect(() => {
+    api.get('api/route/all/places/').then((res) => {
+      setRoutes(Array.from(res.data.results));
+    });
+  }, []);
+  const onFinish = (values: any) => {
+    values.date_of_departure;
     addFlight({
-      id: 's',
       route: selectedRoute,
-      date_of_departure: values.date_of_departure,
+      date_of_departure:
+        values.date_of_departure + 'T' + values.time_of_departure + ':00Z',
       ticket_price: values.ticket_price,
       number_of_seats: values.number_of_seats,
       number_of_free_spaces: values.number_of_seats,
-    })
-      .then((res) => {
-        router.replace('/allFlights');
-      })
-    };
+      id: '',
+    }).then((res) => {
+      router.replace('/allFlights');
+    });
+  };
 
   return (
     <section className={styles.pageWrapper}>
       <ToastContainer />
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Add Flight</h1>
-        <Form
-          form={form}
-          className={styles.flightForm}
-          onFinish={onFinish}
-        >
+        <Form form={form} className={styles.flightForm} onFinish={onFinish}>
           <Form.Item
             name="Route"
-            rules={[
-              { required: true, message: selectedRoute }
-              
-            ]}
+            rules={[{ required: true, message: selectedRoute }]}
           >
-            
             <Select
-                value = {selectedRoute}
-                onChange = {(e)=> setSelectedRoute(e.target)}
+              value={selectedRoute}
+              onChange={(value) => {
+                setSelectedRoute(value);
+                alert(value);
+              }}
             >
-            {Routes.map((route) =>
-            <option value={route.id} key={route.id}>{route.start_point.country}: {route.start_point.airport_city} <ArrowRightOutlined/> {route.end_point.country}: {route.end_point.airport_city}</option>)}
+              {routes.map((route) => (
+                <Select.Option value={route.id} key={route.id}>
+                  {route.start_point.country}: {route.start_point.airport_city}{' '}
+                  <ArrowRightOutlined /> {route.end_point.country}:{' '}
+                  {route.end_point.airport_city}
+                </Select.Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item
             name="date_of_departure"
-            rules={[{ required: true, message: 'Date of departure is required.' }]}
+            rules={[
+              { required: true, message: 'Date of departure is required.' },
+            ]}
           >
             <Input
               className={styles.input}
@@ -75,14 +77,19 @@ const AddFlight = () => {
             />
           </Form.Item>
           <Form.Item
-            name="ticket_price"
+            name="time_of_departure"
             rules={[
-              { required: true, message: 'ticket price is required.' },
-                
+              { required: true, message: 'Time of departure is required.' },
             ]}
           >
+            <Input className={styles.input} type="time" />
+          </Form.Item>
+          <Form.Item
+            name="ticket_price"
+            rules={[{ required: true, message: 'ticket price is required.' }]}
+          >
             <Input
-                type = "number"
+              type="number"
               className={styles.input}
               placeholder="ticket price"
             />
@@ -90,18 +97,21 @@ const AddFlight = () => {
           <Form.Item
             name="number_of_seats"
             rules={[
-              { required: true, message: 'number of seats is required.' }
-              
+              { required: true, message: 'number of seats is required.' },
             ]}
           >
             <Input
-              type = "number"
+              type="number"
               className={styles.input}
               placeholder="number of seats"
             />
-            </Form.Item>
+          </Form.Item>
           <Form.Item className={styles.submit}>
-            <Button type="primary" text="Add Flight" style={{ width: '100%' }} />
+            <Button
+              type="primary"
+              text="Add Flight"
+              style={{ width: '100%' }}
+            />
           </Form.Item>
         </Form>
       </div>
--- src/features/flights/services/flight.service.ts
+++ src/features/flights/services/flight.service.ts
@@ -3,8 +3,8 @@ import Router from 'next/router';
 import FlightDto from '../types/FlightDto';
 
 export const addFlight = async (dto: FlightDto) => {
-    return await api.post('/api/flight/', dto);
-  };
+  return await api.post('/api/flight/', dto);
+};
 export const deleteFlight = async (dto: FlightDto) => {
-  return await api.delete('/api/flight/delete/', { data: dto});
-  };
+  return await api.delete('/api/flight/delete/' + dto.id + '/', { data: dto });
+};
